6c0d95fe90bd6ff42e685d36a31de928ba61652e
Akshit Zaveri <akshit.zaveri@gmail.com>
2020-05-04 11:00:51 +0530

    Creating the problem - done

--- TableViewAdapterExample/ViewController.swift
+++ TableViewAdapterExample/ViewController.swift
@@ -10,7 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 100
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { fatalError() }
+    cell.textLabel?.text = "Row \(indexPath.item + 1)"
+    return cell
+  }
+}
+
+extension ViewController: UITableViewDelegate {
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("Selected row \(indexPath.item + 1)")
+    tableView.deselectRow(at: indexPath, animated: true)
   }
 }
