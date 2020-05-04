f5ee59caf833b8260a703ba2e221f6785c314d12
Akshit Zaveri <akshit.zaveri@gmail.com>
2020-05-04 10:55:00 +0530

    Initial Commit

---
+++ TableViewAdapterExample/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  TableViewAdapterExample
+//
+//  Created by Akshit Zaveri on 04/05/20.
+//  Copyright © 2020 Akshit Zaveri. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let _ = (scene as? UIWindowScene) else { return }
+  }
+}
---
+++ TableViewAdapterExample/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TableViewAdapterExample
+//
+//  Created by Akshit Zaveri on 04/05/20.
+//  Copyright © 2020 Akshit Zaveri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
